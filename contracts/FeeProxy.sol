7c6e97ba0fac4fbcd09073df566fd9a0cc550575
Toufic Batrice <touficbatrice@gmail.com>
2022-06-14 11:52:55 +0200

    solidity version non compatible

--- contracts/FeeProxy.sol
+++ contracts/FeeProxy.sol
@@ -1,12 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6;
 
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol"; 
-import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./IOracle.sol";
+import "./IFeeProxy.sol";
 
-contract FeeProxy is IOracle {
+
+contract FeeProxy is IFeeProxy {
 
     using SafeERC20 for IERC20;
 
@@ -24,21 +25,21 @@ contract FeeProxy is IOracle {
 
     uint24[4] fees = [10000, 3000, 500, 100]; 
 
-    function getLiquidity(address token1, address token2, uint24 fee) internal returns (uint balance1, address poolAddress) {
-        poolAddress = IUniswapV3Factory(factory).getPool(_token0, _token1, _fee);
-        uint balance1;
+    function _getLiquidity(address token1, address token2, uint24 fee) internal returns (uint balance1, address poolAddress) {
+        poolAddress = IUniswapV3Factory(factory).getPool(token1, token2, fee);
+        balance1;
         if (poolAddress != address(0)){
             balance1 = IERC20(token1).balance(poolAddress);
         }
     }
 
-    function maxLiquidity(address token1, address token2) internal returns (uint24 fee, address poolAddress) {
-        uint24 fee = 100;
-        (uint balance, address poolAddresse) = getLiquidity(token1, token2, fee);
+    function _maxLiquidity(address token1, address token2) internal returns (uint24 fee, address poolAddress) {
+        fee = 100;
+        (uint balance, address poolAddresse) = _getLiquidity(token1, token2, fee);
     
         for (uint i; i < 3; i++) {
             uint24 temp_fee = fees[i];
-            (temp_balance, temp_address) = getLiquidity(token1, token2, temp_fee);
+            (uint temp_balance, address temp_address) = _getLiquidity(token1, token2, temp_fee);
             if (temp_balance > balance) {
                 (fee, poolAddress) = (temp_fee, temp_address);
             }
@@ -46,12 +47,12 @@ contract FeeProxy is IOracle {
     }
 
     function update(address token1, address token2) public {
-        (address tokenA, address tokenB) = sortTokens(token1, token2);
-        (param[tokenA][tokenB].poolAddress, param[tokenA][tokenB].fee) = maxLiquidity(tokenA, tokenB);
+        (address tokenA, address tokenB) = _sortTokens(token1, token2);
+        (param[tokenA][tokenB].poolAddress, param[tokenA][tokenB].fee) = _maxLiquidity(tokenA, tokenB);
     }
 
 
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+    function _sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'feeProxy: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'feeProxy: ZERO_ADDRESS');
