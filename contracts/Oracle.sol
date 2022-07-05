1906d339d636e433ba2a20cc8ea3deb38e056de4
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-07-05 11:59:21 +0200

    wip: refactoring functions  and adding setter factory contract function

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -6,30 +6,40 @@ import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 import "./interfaces/IOracle.sol";
 import "./interfaces/IBalanceGetterERC20.sol";
 
-import "debond-governance/contracts/utils/GovernanceOwnable.sol";
+import "debond-governance-contracts/utils/GovernanceOwnable.sol";
 
 contract Oracle is IOracle , GovernanceOwnable {
-    
     address immutable factory;
-    mapping( address => mapping( address => address) ) poolAddresses;
+    mapping(address => mapping( address => address)) poolAddresses;
     uint24[4] fees = [10000, 3000, 500, 100];
-   
+    uint24 liquidityFeesParam = 100;
     constructor(
         address _factory,
         address governance
     ) GovernanceOwnable(governance) {
         factory = _factory;
     }
-
-    function update(address token1, address token2) external onlyGovernance {
+    /**
+    updating the liquidity of the pool address, based on the availablity of the corresponding pool address.    
+     */
+    function updateLiquidity(address token1, address token2) external  {
         (address tokenA, address tokenB) = _sortTokens(token1, token2);
         poolAddresses[tokenA][tokenB] = _maxLiquidity(tokenA, tokenB);
     }
+    /**
+    internal function  to determine pool address with the maxLiquidity for the corresponding pair 
+    @dev in order to use them for determining the price which is consistent for sufficient liquidity.   
+    @param token1 is the address of given ERC20.
+    @param token2 is the address of underlying stablecoin.
+    @returns poolAddress the address of pool with the highest liquidity.
+     */
 
     function _maxLiquidity(address token1, address token2) internal view returns (address poolAddress) {
         uint balance;
-        (balance, poolAddress) = _getLiquidity(token1, token2, 100);
+        //TODO: the  fees parameter will not be constant, will depend on the given pool. 
+        (balance, poolAddress) = _getLiquidity(token1, token2, liquidityFeesParam);
 
+        // determining the minimum fees sufficient for getting the pool address.
         for (uint i; i < 3; i++) {
             (uint temp_balance, address temp_address) = _getLiquidity(token1, token2, fees[i]);
             if (temp_balance > balance) {
@@ -38,8 +48,16 @@ contract Oracle is IOracle , GovernanceOwnable {
         }
     }
 
+
+    /**
+    @dev getting balance of ERC20 present in the pool (for the swap conversion). 
+    @param token1 is the address of given ERC20.
+    @param token2 is the address of underlying stablecoin.
+
+
+     */
     function _getLiquidity(address token1, address token2, uint24 fee) internal view returns (uint balance1, address poolAddress) {
-        poolAddress = getPoolWithoutCheck(token1, token2, fee);
+        poolAddress = _getPoolWithoutCheck(token1, token2, fee);
         if (poolAddress != address(0)){
             balance1 = IBalanceGetterERC20(token1).balanceOf(poolAddress);
         }
@@ -51,14 +69,14 @@ contract Oracle is IOracle , GovernanceOwnable {
         require(token0 != address(0), 'feeProxy: ZERO_ADDRESS');
     }
     
-    function getPool(address _token0, address _token1, uint24 _fee) internal view returns(address poolAddress) {
-         poolAddress = IUniswapV3Factory(factory).getPool(
-            _token0,
-            _token1,
-            _fee
-        );
-        require(poolAddress != address(0), "pool doesn't exist");
-    }
+    // function getPool(address _token0, address _token1, uint24 _fee) internal view returns(address poolAddress) {
+    //      poolAddress = IUniswapV3Factory(factory).getPool(
+    //         _token0,
+    //         _token1,
+    //         _fee
+    //     );
+    //     require(poolAddress != address(0), "pool doesn't exist");
+    // }
      
 
     function estimateAmountOut(
@@ -96,15 +114,9 @@ contract Oracle is IOracle , GovernanceOwnable {
 
         // int56 / uint32 = int24
         int24 tick = int24(tickCumulativesDelta / secondsAgo);
-        // Always round to negative infinity
-        /*
-        int doesn't round down when it is negative
-        int56 a = -3
-        -3 / 10 = -3.3333... so round down to -4
-        but we get
-        a / 10 = -3
-        so if tickCumulativeDelta < 0 and division has remainder, then round
-        down
+     
+        /* if tickCumulativeDelta < 0 and division has remainder
+       using floor function for decreasing price: we convert into the previous least integer. 
         */
         if (
             tickCumulativesDelta < 0 && (tickCumulativesDelta % secondsAgo != 0)
@@ -119,8 +131,22 @@ contract Oracle is IOracle , GovernanceOwnable {
             tokenOut
         );
     }
-    //for changing fee param
-    function getPoolWithoutCheck(address token1, address token2, uint24 fee) public override view returns (address poolAddress) {
+    /**
+    @dev for fetching the pool address for the given pool and fees 
+    @dev  used as an check for determining liquidity .  
+     */
+    function _getPoolWithoutCheck(address token1, address token2, uint24 fee) internal override view returns (address poolAddress) {
         poolAddress = IUniswapV3Factory(factory).getPool(token1, token2, fee);  //can be 0
     }
+
+    /**
+    changing factory contract when needed.
+     */
+    function setFactoryContract(address _newFactoryContract) external onlyGovernace returns(bool){
+        _factory = _newFactoryContract;
+        return(true);
+
+    }
+
+
 }
